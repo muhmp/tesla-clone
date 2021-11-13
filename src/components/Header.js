95f1463527c411c76fd4ec1b845318d7fa5e07f3
muhmp <defcoolz@gmail.com>
2021-11-13 20:44:25 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
+import Menu from '@mui/material/Menu';
 
 function Header() {
     return (
         <Container>
-                <a>
-                    <img src="/images/logo.svg" alt="" />
-                </a>
+            <a>
+                <img src="/images/logo.svg" alt="" />
+            </a>
 
-                <Menu>
-                    <p><a href="#">Model S</a></p>
-                    <p><a href="#">Model 3</a></p>
-                    <p><a href="#">Model X</a></p>
-                    <p><a href="#">Model Y</a></p>
-                </Menu>
+            <navMenu>
+                <a href="#">Model S</a>
+                <a href="#">Model 3</a>
+                <a href="#">Model X</a>
+                <a href="#">Model Y</a>
+            </navMenu>
+
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+            </RightMenu>
+
 
         </Container>
      
@@ -29,9 +36,28 @@ const Container = styled.div `
     display:flex;
     align-items: center;
     padding: 0 20px;
+    top: 0;
+    left:0;
+    right:0;
 `
 
-const Menu = styled.div `
+const navMenu = styled.div`
     display:flex;
     align-items:center;
+    justify-content: center;
+    flex:1;
+    a{
+        font-weight:600;
+        text-transform: uppercase;
+        padding: 0 10px; /*top&bottom = 0; right&left = 10px;*/
+        flex-wrap: nowrap;
+    }
+`
+
+const RightMenu = styled.div `
+    a{
+        font-weight:600;
+        text-transform: uppercase;
+        margin-right:10px;
+        }
 `
